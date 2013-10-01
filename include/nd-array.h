a86f339cc4b6d21a3b52d801b8440e37e0ff5d4c
ylecuyer <yoann.lecuyer@gmail.com>
2013-09-30 19:51:06 -0500

    Added missing parenthesis

--- include/nd-array.h
+++ include/nd-array.h
@@ -1,10 +1,10 @@
 #ifndef _ND_ARRAY_H_
 #define _ND_ARRAY_H_
 
-#define ACCESS1(tab, XSIZE, x) (*(tab + x))
-#define ACCESS2(tab, XSIZE, YSIZE, x, y) (*(tab + x + XSIZE*y))
-#define ACCESS3(tab, XSIZE, YSIZE, ZSIZE, x, y, z) (*(tab + x + XSIZE*(y + YSIZE*z)))
-#define ACCESS4(tab, XSIZE, YSIZE, ZSIZE, JSIZE, x, y, z, j) (*(tab + x + XSIZE*(y + YSIZE*(z + ZSIZE*j))))
-#define ACCESS5(tab, XSIZE, YSIZE, ZSIZE, JSIZE, KSIZE, x, y, z, j, k) (*(tab + x + XSIZE*(y + YSIZE*(z + ZSIZE*(j + JSIZE*k)))))
+#define ACCESS1(tab, XSIZE, x) (*(tab + (x)))
+#define ACCESS2(tab, XSIZE, YSIZE, x, y) (*(tab + (x) + (XSIZE)*(y)))
+#define ACCESS3(tab, XSIZE, YSIZE, ZSIZE, x, y, z) (*(tab + (x) + (XSIZE)*((y) + (YSIZE)*(z))))
+#define ACCESS4(tab, XSIZE, YSIZE, ZSIZE, JSIZE, x, y, z, j) (*(tab + (x) + (XSIZE)*((y) + (YSIZE)*((z) + (ZSIZE)*(j)))))
+#define ACCESS5(tab, XSIZE, YSIZE, ZSIZE, JSIZE, KSIZE, x, y, z, j, k) (*(tab + (x) + (XSIZE)*((y) + (YSIZE)*((z) + (ZSIZE)*((j) + (JSIZE)*(k))))))
 
 #endif
